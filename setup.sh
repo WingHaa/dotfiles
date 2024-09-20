4012450bb28be54130a060e72fcdcabb7aa6ba49
WingHaa <quanlm24@gmail.com>
2024-09-20 23:38:44 +0700

    update stow setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,19 @@
- stow -vR *
+#!/bin/bash
+
+# Check if GNU Stow is installed
+if ! command -v stow &> /dev/null; then
+    echo "GNU Stow is not installed. Please install it first."
+    exit 1
+fi
+
+# Get a list of all directories in the current directory
+directories=$(find . -maxdepth 1 -type d ! -name '.')
+
+# Loop through each directory and stow it
+for dir in $directories; do
+    echo "Stowing $dir..."
+    stow -vSR "$(basename "$dir")"
+done
+
+echo "All directories have been stowed."
+
