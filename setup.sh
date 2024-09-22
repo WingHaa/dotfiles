1eaed8d88fdc4d058ad1d8d7c6729003e4b68d5a
WingHaa <quanlm24@gmail.com>
2024-09-22 22:25:59 +0700

    update waybar

--- setup.sh
+++ setup.sh
@@ -6,23 +6,4 @@ if ! command -v stow &> /dev/null; then
     exit 1
 fi
 
-# Define the list of directories to ignore
-ignores=("setup.sh" "scripts" ".git" ".gitmodules" ".stow-global-ignore")
-
-# Build the find command with the ignore conditions
-ignore_patterns=""
-for item in "${ignores[@]}"; do
-    ignore_patterns+=" -not -name '$item'"
-done
-
-# Get a list of all directories in the current directory
-directories=$(eval "find . -maxdepth 1 ! -name '.' $ignore_patterns")
-
-# Loop through each directory and stow it
-for dir in $directories; do
-    echo "Stowing $dir..."
-    stow -vR "$(basename "$dir")"
-done
-
-echo "All directories have been stowed."
-
+stow -vR "apps"
