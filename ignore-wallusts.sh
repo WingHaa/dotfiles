445bf272bbac9a4dad0f2ee9135d2b928433aaa0
WingHaa <quanlm24@gmail.com>
2024-09-25 08:12:38 +0700

    update ignore list & script

---
+++ ignore-wallusts.sh
@@ -0,0 +1,5 @@
+git update-index --skip-worktree apps/.config/hypr/themes/wallust.conf
+git update-index --skip-worktree apps/.config/rofi/.current_wallpaper
+git update-index --skip-worktree apps/.config/rofi/shared/colors.rasi
+git update-index --skip-worktree apps/.config/swaync/themes/wallust/colors-wallust.css
+git update-index --skip-worktree apps/.config/waybar/colors.css
