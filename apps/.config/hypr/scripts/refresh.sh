f40e54c368b9caee6f3a7909bdbb22a61dd0b941
WingHaa <quanlm24@gmail.com>
2025-12-30 09:57:13 +0700

    update conf

--- apps/.config/hypr/scripts/refresh.sh
+++ apps/.config/hypr/scripts/refresh.sh
@@ -2,17 +2,6 @@
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 # Scripts for refreshing ags waybar, rofi, swaync, wallust
 
-SCRIPTSDIR=$HOME/.config/hypr/scripts
-
-# Define file_exists function
-file_exists() {
-    if [ -e "$1" ]; then
-        return 0  # File exists
-    else
-        return 1  # File does not exist
-    fi
-}
-
 # Kill already running processes
 _ps=(waybar rofi swaync ags)
 for _prs in "${_ps[@]}"; do
@@ -28,12 +17,5 @@ sleep 0.3
 # relaunch swaync
 sleep 0.5
 swaync > /dev/null 2>&1 &
-
-# Relaunching rainbow borders if the script exists
-sleep 1
-if file_exists "${SCRIPTSDIR}/rainbow-border.sh"; then
-    ${SCRIPTSDIR}/rainbow-border.sh &
-fi
-
 
 exit 0
